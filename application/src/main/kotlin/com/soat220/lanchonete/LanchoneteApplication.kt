88784dcdb77bcb815289251f9da1e2187b74c9a6
Marcos Moreira <marquinhos2725@hotmail.com>
2024-02-01 20:45:15 -0300

    chore: testing actions

--- application/src/main/kotlin/com/soat220/lanchonete/LanchoneteApplication.kt
+++ application/src/main/kotlin/com/soat220/lanchonete/LanchoneteApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class LanchoneteApplication
 
+// Hello world
+
 fun main(args: Array<String>) {
 	runApplication<LanchoneteApplication>(*args)
 }
